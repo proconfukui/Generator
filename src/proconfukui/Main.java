f49cb75ec81b2e00c871cf1fc321ac77a9fc099a
うたうたげ <kotalegoblock@gmail.com>
2025-06-20 09:22:00 +0900

    プログラムを変更

--- src/proconfukui/Main.java
+++ src/proconfukui/Main.java
@@ -10,18 +10,9 @@ import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 
-class X {
-	int startsAt;
-	static class Problem {
-		static class Field {
-			int size;
-			int[][] entities;
-		}
-	}
-}
-
 public class Main {
 	static Scanner scanner = new Scanner(System.in);
+	static final String path = "/Users/utautage/output.json"; // 環境に応じて書き換える
 	public static void main(String[] args) throws IOException {
 		int size = 0;
 		do {
@@ -31,16 +22,12 @@ public class Main {
 				size = Integer.parseInt(input);
 			} catch (NumberFormatException e) {}
 		} while (size % 2 != 0 || size < 4 || size > 24);
-		System.out.println();
-		
 		ArrayList<Integer> entities = new ArrayList<Integer>(size * size);
 		for (int number = 1; number <= size * size / 2; number++) {
 			entities.add(number);
 			entities.add(number);
 		}
 		Collections.shuffle(entities);
-		
-		String path = "/Users/utautage/output.json"; // 環境に応じて書き換える
 		JsonGenerator generator = new JsonFactory().createGenerator(new File(path), JsonEncoding.UTF8);
 		generator.useDefaultPrettyPrinter();
 		generator.writeStartObject();
@@ -55,14 +42,10 @@ public class Main {
 		for (int y = 0; y < size; y++) {
 			generator.writeStartArray();
 			for (int x = 0; x < size; x++) {
-				int number = entities.get(y * size + x);
-				generator.writeNumber(number);
-				System.out.print(String.format("% 4d", number));
+				generator.writeNumber(entities.get(y * size + x));
 			}
 			generator.writeEndArray();
-			System.out.println();
 		}
-		System.out.println();
 		generator.writeEndArray();
 		generator.writeEndObject();
 		generator.writeEndObject();
